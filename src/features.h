0005112d783009e41833681c72d05f90b621418f
thomas-berge <thomas.berge@etu.estia.fr>
2025-05-26 17:18:27 +0200

    rajoute min_pixel

--- src/features.h
+++ src/features.h
@@ -2,13 +2,16 @@
 #define FEATURES_H
 
 
+
 void helloWorld();
 void dimension (char *source_path);
 void first_pixel(char *source_path);
 void second_line(char *source_path);
 void tenth_pixel (char *source_path);
 void print_pixel(char *filename, int x, int y);
-void max_pixel(char *source_path); 
-void min_pixel(char *source_path);
+void max_pixel(char *source_path);
+void min_pixel(char *source_path); 
+void max_component(char *source_path, char component);
+
 
 #endif
