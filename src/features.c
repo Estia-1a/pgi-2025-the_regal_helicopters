dd83c0468f835ce639f8da300a6092d13b5a82e9
matthiasb64 <matthias.boyer@etu.estia.fr>
2025-06-18 11:01:46 +0200

    color blue

--- src/features.c
+++ src/features.c
@@ -328,3 +328,22 @@ void color_green(char *source_path) {
     }
     write_image_data("image_out.bmp", new_data, width, height);
 }
+void color_blue(char *source_path) {
+    int width, height, channel_count;
+    unsigned char *data;
+
+    read_image_data(source_path, &data, &width, &height, &channel_count);
+
+    unsigned char *new_data = (unsigned char*)malloc(width * height * channel_count * sizeof(unsigned char));
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            pixelRGB* pixel = get_pixel(data, width, height, channel_count, x, y);
+            unsigned char blue  = pixel->B ;
+            new_data[(y * width + x) * channel_count] = 0;
+            new_data[(y * width + x) * channel_count + 1] = 0 ;
+            new_data[(y * width + x) * channel_count + 2] = blue;
+        }
+    }
+    write_image_data("image_out.bmp", new_data, width, height);
+}
